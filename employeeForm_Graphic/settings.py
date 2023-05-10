35f3d6da250e1d7c99a832386b0ec2f8cae8f6d0
Onica Mihail <onicamihail2002@gmail.com>
2023-05-11 00:44:28 +0300

    Short commit | .gitignore and settings.py
    1>Changed gitigore to ignore vs code

--- employeeForm_Graphic/settings.py
+++ employeeForm_Graphic/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d-a3yne9wn^jg(s@m&=u6pk-o7xmfs_3)cdl8$hjsnxr10nxi2'
+SECRET_KEY = 'django-insecure-d-a3yne9^jg(s@m&=uggh6pk-o7xmfs_3)cdl8$hjsnxr10nxi2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,9 +74,14 @@ WSGI_APPLICATION = 'employeeForm_Graphic.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_name',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
