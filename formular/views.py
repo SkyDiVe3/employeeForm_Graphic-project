1b2a9b0e2f5f61ee3652a39ef23174072b88bf84
Onica Mihail <onicamihail2002@gmail.com>
2023-05-14 16:07:46 +0300

    Testing Model Change and Data Visualization Template
    Testing Model Change:
    
    Modified the testing model in the Django app.
    Made necessary adjustments to the model fields or structure.
    Ran the database migrations using python manage.py makemigrations and python manage.py migrate to apply the changes to the database schema.
    Data Visualization Template:
    
    Created a new template to visualize data from the MySQL database.
    Designed the template to display the data in a visually appealing manner.
    Connected the template to the appropriate URL mapping in urls.py to make it accessible via a specific URL.
    Added customization to the template, such as styling, formatting, or interactive elements, to enhance the user experience

--- formular/views.py
+++ formular/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+
+def home(request):
+    projects  =  Project.objects.all()
+    projects_with_numbers = [(i+1, obj) for i, obj in enumerate(projects)]
+
+    return render(request, '../templates/formular/home.html', {"projects" : projects_with_numbers})
